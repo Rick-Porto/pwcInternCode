a81a4410e64242173043742dc292b07fa512c0f6
Rick Porto <rickmarco4@hotmail.com>
2023-07-08 17:08:05 -0300

    Challenge 01 Reverse String

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,19 @@
 package application;
 
+import entities.StringManipulation;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Desafio 01
+	    String input1 = "Hello, World! OpenAI is amazing.";
+	    String reversedWords = StringManipulation.reverseWords(input1);
+	    System.out.println(reversedWords);
+	    
+	    //Desafio 02
+	    
+	    //Desafio 03
+	    
 	}
-
 }
--- src/entities/StringManipulation.java
+++ src/entities/StringManipulation.java
@@ -2,4 +2,18 @@ package entities;
 
 public class StringManipulation {
 
+	// Reverter a ordem das palavras na frase - Desafio 01
+	public static String reverseWords(String input) {
+		String[] words = input.split(" ");
+		StringBuilder reversed = new StringBuilder();
+
+		for (int i = words.length - 1; i >= 0; i--) {
+			reversed.append(words[i]).append(" ");
+		}
+
+		return reversed.toString().trim();
+	}
+	
+	
+
 }
